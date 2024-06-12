47e887044d41d4fb3e7dfa958b5845c8987c19bb
Aathithya Tiraviam S <125111716+AathiTira@users.noreply.github.com>
2024-06-12 18:48:24 +0530

    Week-1 assignment added

--- Week 1/Medium/countVowels.js
+++ Week 1/Medium/countVowels.js
@@ -1,5 +1,4 @@
-/*
-  Implement a function `countVowels` that takes a string as an argument and returns the number of vowels in the string.
+/*Implement a function `countVowels` that takes a string as an argument and returns the number of vowels in the string.
   Note: Consider both uppercase and lowercase vowels ('a', 'e', 'i', 'o', 'u').
 
   Once you've implemented the logic, test your code by running
