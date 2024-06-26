c7754ce57a58598977c52d547be6bec4d769c8a2
Aathithya Tiraviam S <125111716+AathiTira@users.noreply.github.com>
2024-06-26 09:04:21 +0530

    Week-1 assignment added

--- Week 1/Medium/countVowels.js
+++ Week 1/Medium/countVowels.js
@@ -1,8 +1,7 @@
 /*Implement a function `countVowels` that takes a string as an argument and returns the number of vowels in the string.
   Note: Consider both uppercase and lowercase vowels ('a', 'e', 'i', 'o', 'u').
   Once you've implemented the logic, test your code by running
-  To Run file -> PS C:\Users\chand\Desktop\100xdevs_2.0\0-1\week1\assignments\mediun> npx jest ./tests/countVowels.test.js
-  */
+  To Run file -> PS C:\Users\chand\Desktop\100xdevs_2.0\0-1\week1\assignments\mediun> npx jest ./tests/countVowels.test.js */
 
   function countVowels(str) {
     let vowels = ['a', 'e', 'i', 'o', 'u'];
