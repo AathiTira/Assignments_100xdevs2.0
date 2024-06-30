2156cf3c476db571411f638fca567e87e0bd1e7e
Aathithya Tiraviam S <125111716+AathiTira@users.noreply.github.com>
2024-06-30 12:07:31 +0530

    Week-3 assignment added

--- Week 3/MIddlewares/03-errorCount.js
+++ Week 3/MIddlewares/03-errorCount.js
@@ -23,7 +23,8 @@ app.get('/errorCount', function(req, res) {
 /* You have been given an express server which has a few endpoints.
 Your task is to
 1. Ensure that if there is ever an exception, the end user sees the status code of 404
-2. Maintain the errorCount variable whose value should go up every time there is an exception in any endpoint */
+2. Maintain the errorCount variable whose value should go up every time there is an exception in any endpoint 
+*/
 
 // error handling middleware
 
