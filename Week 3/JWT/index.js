82c6cf05ee5d76dacc774af4b0f6821db1ba9525
Aathithya Tiraviam S <125111716+AathiTira@users.noreply.github.com>
2024-07-01 01:47:37 +0530

    Week-3 assignment added

--- Week 3/JWT/index.js
+++ Week 3/JWT/index.js
@@ -12,8 +12,7 @@ const passwordSchema = zod.string().min(6);
  * @param {string} password - The password to be included in the JWT payload.
  *                            Should meet the defined length requirement (e.g., 6 characters).
  * @returns {string|null} A JWT string if the username and password are valid.
- *                        Returns null if the username is not a valid email or
- *                        the password does not meet the length requirement. 
+ *                        Returns null if the username is not a valid email or the password does not meet the length requirement. 
  */
 
 function signJwt(username, password) {
