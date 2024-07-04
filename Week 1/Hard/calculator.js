91fb3efa7cd33587f46ffea76466c941eb28ad32
Aathithya Tiraviam S <125111716+AathiTira@users.noreply.github.com>
2024-07-04 12:27:23 +0530

    Week-1 assignment added

--- Week 1/Hard/calculator.js
+++ Week 1/Hard/calculator.js
@@ -12,8 +12,7 @@
         1. the input can have multiple continuous spaces, you're supposed to avoid them and parse the expression correctly
         2. the input can have invalid non-numerical characters like `5 + abc`, you're supposed to throw error for such inputs
 
-  Once you've implemented the logic, test your code by running
-*/
+  Once you've implemented the logic, test your code by running */
 
 class Calculator {
   // initialise a result variable in the constructor and keep updating it after every arithmetic operation
