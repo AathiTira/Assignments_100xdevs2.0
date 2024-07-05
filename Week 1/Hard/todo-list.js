45cf80b36f02d196a5a4c6fb28ee8cb37545bf46
Aathithya Tiraviam S <125111716+AathiTira@users.noreply.github.com>
2024-07-06 00:18:10 +0530

    Week-1 assignment added

--- Week 1/Hard/todo-list.js
+++ Week 1/Hard/todo-list.js
@@ -1,4 +1,5 @@
-/*Implement a class `Todo` having below methods
+/*
+Implement a class `Todo` having below methods
     - add(todo): adds todo to list of todos
     - remove(indexOfTodo): remove todo from list of todos
     - update(index, updatedTodo): update todo at given index
