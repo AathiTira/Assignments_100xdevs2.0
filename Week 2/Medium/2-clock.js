0402b8526673e8dbf732d77268f5aaea4ae32a89
Aathithya Tiraviam S <125111716+AathiTira@users.noreply.github.com>
2024-07-07 01:09:14 +0530

    Week-2 assignment added

--- Week 2/Medium/2-clock.js
+++ Week 2/Medium/2-clock.js
@@ -1,7 +1,8 @@
 /* Using 1-counter.js or 2-counter.js from the easy section, can you create a clock that shows you the current machine time?
 Can you make it so that it updates every second, and shows time in the following formats -
 HH:MM::SS (Eg. 13:45:23)
-HH:MM::SS AM/PM (Eg 01:45:23 PM) */
+HH:MM::SS AM/PM (Eg 01:45:23 PM) 
+*/
 
 function padZero(num) {
   return num.toString().padStart(2, '0');
