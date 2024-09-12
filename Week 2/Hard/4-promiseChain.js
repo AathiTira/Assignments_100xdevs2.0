2516b92dcfd580ef18cde27e2d3fefb5f0c85f3e
Aathithya Tiraviam S <125111716+AathiTira@users.noreply.github.com>
2024-09-12 13:22:24 +0530

    Week-2 assignment added

--- Week 2/Hard/4-promiseChain.js
+++ Week 2/Hard/4-promiseChain.js
@@ -2,7 +2,8 @@
  * Write 3 different functions that return promises that resolve after t1, t2, and t3 seconds respectively.
  * Write a function that sequentially calls all 3 of these functions in order.
  * Return a promise chain which return the time in milliseconds it takes to complete the entire operation.
- * Compare it with the results from 3-promise-all.js  */
+ * Compare it with the results from 3-promise-all.js  
+ */
 
 // Function that returns a promise that resolves after t1 seconds
 function waitForT1(t1) {
